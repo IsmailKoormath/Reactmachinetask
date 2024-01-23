06543d8e2d8d33587a4798c0b97b16ea897b60ce
ismail <ismailkoormath037@gmail.com>
2024-01-23 13:04:00 +0530

    hero section top completed

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import LineChart from '../../Pages/Home/Components/LineChart/LineChart'
 import classes from './Hero.module.css'
+
+import profile from '../../Assets/Rectangle 10.png'
+import smile from '../../Assets/smile.png'
 const Hero = () => {
   return (
     <div className={classes.Hero_section}>
-      <div>
-      <h4>Good Monrning ! </h4>
-      <div>
-        <div>
-          <h6></h6>
-          <span></span>
+      <div className={classes.hero_top}>
+        <h4>Good Monrning ! <img src={smile} alt="smile" /></h4>
+        <div className={classes.profile}>
+          <div className={classes.profileDetails}>
+            <h6>John Doe</h6>
+            <span>john@doe.com</span>
+          </div>
+          <img src={profile} alt="profile" />
         </div>
-        <img src="" alt="profile" />
       </div>
-      </div>
-        <LineChart/>
+      <LineChart />
     </div>
   )
 }
